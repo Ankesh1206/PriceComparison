992e906099dfa16569ad78e289c0abd04ac09f62
paryanshu <paryanshusourav@outlook.com>
2020-02-20 12:50:25 +0530

    Removing locators

--- src/main/java/ApplicationUtilities/ApplicationObjects.java
+++ src/main/java/ApplicationUtilities/ApplicationObjects.java
@@ -7,8 +7,6 @@ public class ApplicationObjects {
     public static final By loginCloseButton= By.xpath("//button[@class='_2AkmmA _29YdH8']");
     public static final By flipkartSearchBox=By.name("q");
     public static final By flipkartSearchButton=By.xpath("//button[@class='vh79eN']");
-    public static final By flipkartItemSection=By.xpath("//div[@class='_1UoZlX']");
-    public static final By flipkartItemNameSection=By.xpath("//div[@class='_3wU53n']");
     public static final By flipkartItemPriceSection=By.xpath("//div[@class='_1vC4OE _2rQ-NK']");
 
 
